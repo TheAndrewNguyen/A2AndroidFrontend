6936b2f5dc97cfd8a5ae120e275e8d8a1dd69836
Andrew Nguyen <andrewbvietnguyen@gmail.com>
2024-10-10 13:54:19 -0700

    yolo

--- app/src/main/java/com/example/a2chat2nd/JoinScreen.kt
+++ app/src/main/java/com/example/a2chat2nd/JoinScreen.kt
@@ -2,9 +2,10 @@ package com.example.a2chat2nd
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
 
 @Composable
-fun JoinScreen() {
+fun JoinScreen(navController: NavController) {
     Text("this is the join screen")
 }
 
