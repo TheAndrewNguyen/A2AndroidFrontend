e175574752ef7854426360a46980a285848eb037
Andrew Nguyen <andrewbvietnguyen@gmail.com>
2024-10-10 13:55:40 -0700

    added navigation to all pages

--- app/src/main/java/com/example/a2chat2nd/CreateScreen.kt
+++ app/src/main/java/com/example/a2chat2nd/CreateScreen.kt
@@ -4,6 +4,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
+//yay
+
 @Composable
 fun CreateScreen(navController: NavController) {
     Text("this is the create screen")
