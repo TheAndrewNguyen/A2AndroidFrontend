f746e14e919eeb980b9216e380cf6949937b4038
Andrew Nguyen <andrewbvietnguyen@gmail.com>
2024-10-17 08:56:47 -0700

    create screen MVP UI finished

--- app/src/main/java/com/example/a2chat2nd/CreateScreen.kt
+++ app/src/main/java/com/example/a2chat2nd/CreateScreen.kt
@@ -1,34 +1,40 @@
 package com.example.a2chat2nd
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
-/*TODO: features:
- - navigation: end button --> confirm leave
- - design:
-  - top: join code end button
-  - middle: chat screen
-  - bottom: message box, send button
-*/
 
 @Composable
 fun CreateScreen(navController: NavController) {
@@ -40,14 +46,12 @@ fun CreateScreen(navController: NavController) {
             .padding(16.dp)
     ) {
         TopStrip(navController, showPopup)
+        MessageDisplay()
         if (showPopup.value) {
             EndPopUp(navController, showPopup)
         }
     }
 }
-
-
-
 
 //top strip components
 @Composable
@@ -109,7 +113,96 @@ fun EndPopUp(navController: NavController, showPopUp: MutableState<Boolean>) {
     )
 }
 
+//messages
+@Composable
+fun Message(message: String) {
+    Box(
+        modifier = Modifier
+            .wrapContentSize()
+            .clip(shape = RoundedCornerShape(30.dp))
+            .background(colorResource(R.color.appleblue))
+            .padding(15.dp)
+    ) {
+        Column {
+            Text(
+                text = message,
+                color = colorResource(R.color.white),
+                modifier = Modifier.padding(bottom = 4.dp)
+            )
+        }
+    }
+}
 
+@Composable
+fun MessageDisplay() {
+    val scrollState = rememberScrollState()
 
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+    ) {
+        Column (
+            modifier = Modifier
+                .verticalScroll(scrollState)
+                .fillMaxWidth()
+                .weight(1f)
+                .padding(8.dp),
+            horizontalAlignment = Alignment.End,
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            Message("Yolo3")
+            Message("Yolo1")
+            Message("ALEX GET THE DRUGS ")
+            Message("ALEX WHERE ARE THE DRUGS!!!")
+            Message("ALEX GIMMIE THE DRUGS!!!!!!1")
+            Message("ALEX GIMMIE THE DRUGS!!!!!!1")
+            Message("ALEX GIMMIE THE DRUGS!!!!!!1")
+            Message("ALEX GIMMIE THE DRUGS!!!!!!1")
+            Message("ALEX GIMMIE THE DRUGS!!!!!!1")
+            Message("ALEX GIMMIE THE DRUGS!!!!!!1")
+            Message("ALEX GIMMIE THE DRUGS!!!!!!1")
+            Message("ALEX GIMMIE THE DRUGS!!!!!!1")
+        }
+        MessageInput()
+    }
+}
+
+@Composable
+fun MessageInput() {
+    val message = remember { mutableStateOf("") }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        TextField(
+            value = message.value,
+            onValueChange = { message.value = it },
+            label = { Text("Message") },
+            modifier = Modifier
+                .weight(1f)
+                .onFocusChanged { focusState->
+                    if(focusState.isFocused) {
+                        keyboardController?.show()
+                    }
+                }
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Button(
+            onClick = { /*TODO */ },
+            modifier = Modifier.weight(0.4f)
+        ) {
+            Text("Send")
+        }
+    }
+}
 
 
